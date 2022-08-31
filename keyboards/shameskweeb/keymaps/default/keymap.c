758cbff0377573034b1f8e0e8269907a337aa58b
ShamesTik <32077513+ShamesTik@users.noreply.github.com>
2022-08-31 18:33:43 +0200

    '2x2' split test

--- keyboards/shameskweeb/keymaps/default/keymap.c
+++ keyboards/shameskweeb/keymaps/default/keymap.c
@@ -8,25 +8,80 @@ enum layers{
   _ADJ
 };
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) { /* First encoder */
+        if (clockwise) {
+            tap_code_delay(KC_VOLU, 10);
+        } else {
+            tap_code_delay(KC_VOLD, 10);
+        }
+    }
+//    else if (index == 1) { /* Second encoder */
+//        if (clockwise) {
+//            rgb_matrix_increase_hue();
+//        } else {
+//            rgb_matrix_decrease_hue();
+//        }
+//    }
+    return false;
+}
+void keyboard_post_init_user(void) {
+  // Customise these values to desired behaviour
+  debug_enable=true;
+  debug_matrix=true;
+  //debug_keyboard=true;
+  //debug_mouse=true;
+}
 
-	[_QWERTY] = LAYOUT(
-        LT(_FN,KC_1), KC_2, KC_3,
-        KC_4, KC_5, KC_6,
-        KC_7, KC_8, KC_9
-		),
-	[_FN] = LAYOUT(
-        KC_1, KC_2, KC_3,
-        KC_4, KC_5, KC_6,
-        TO(_QWERTY), TO(_ADJ), KC_0
-		),
-	[_ADJ] = LAYOUT(
-        KC_1, KC_2, KC_3,
-        KC_4, KC_5, KC_6,
-        TO(_QWERTY), TO(_FN), KC_0
-		)
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  // If console is enabled, it will print the matrix position and status of each key pressed
+#ifdef CONSOLE_ENABLE
+    uprintf("KL: kc: 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
+#endif
+  return true;
+}
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_QWERTY] = LAYOUT(
+        KC_1, KC_2,
+        KC_3, KC_4,
+        KC_5, KC_6,
+        KC_7, KC_8
+    )
+//    [_FN] = LAYOUT(
+//        KC_1, KC_2,
+//        QK_BOOT,KC_5,
+//        KC_4, KC_5,
+//        TO(_QWERTY), TO(_ADJ)
+//    ),
+//    [_ADJ] = LAYOUT(
+//        KC_1, KC_2,
+//        KC_4, KC_5,
+//        KC_4, KC_5,
+//        TO(_QWERTY), TO(_FN)
+//    )
 };
 
+//const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+//[_QWERTY] = LAYOUT(
+//    LT(_FN,KC_1), KC_2, KC_3,
+//    KC_4, KC_5, KC_6,
+//    KC_4, KC_5, KC_6,
+//    KC_1, KC_2, KC_3
+//),
+//[_FN] = LAYOUT(
+//    KC_1, KC_2, QK_BOOT,
+//    KC_4, KC_5, KC_6,
+//    KC_4, KC_5, KC_6,
+//    TO(_QWERTY), TO(_ADJ), KC_1
+//),
+//[_ADJ] = LAYOUT(
+//    KC_1, KC_2,KC_3,
+//    KC_4, KC_5, KC_6,
+//    KC_1, KC_2, KC_3,
+//    TO(_QWERTY), TO(_FN), KC_1
+//)
+//};
+//
 #ifdef OLED_ENABLE
 bool oled_task_user(void) {
     // Host Keyboard Layer Status
@@ -55,4 +110,5 @@ bool oled_task_user(void) {
 
     return false;
 }
+
 #endif
--- keyboards/shameskweeb/rules.mk
+++ keyboards/shameskweeb/rules.mk
@@ -1,5 +1,6 @@
 # MCU name
 MCU = STM32F411
+BOARD = BLACKPILL_STM32_F411
 
 # Bootloader selection
 BOOTLOADER = stm32-dfu
@@ -10,14 +11,17 @@ BOOTLOADER = stm32-dfu
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
+CONSOLE_ENABLE = yes 		# Console for debug
+COMMAND_ENABLE = yes         # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
+KEYBOARD_SHARED_EP = yes    # Free up some extra endpoints - needed if console+mouse+extra
+ENCODER_ENABLE = yes        # Enable Rotary Encoder
 
-# SPLIT_KEYBOARD = yes
+SPLIT_KEYBOARD = yes
+SERIAL_DRIVER = usart
 
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306
