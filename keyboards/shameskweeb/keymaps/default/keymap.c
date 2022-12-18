1ecb3138fbea978313cff809ffe6bcda47cab925
ShamesTik <32077513+ShamesTik@users.noreply.github.com>
2022-12-18 12:00:13 +0100

    Removed Unicode Layer

--- keyboards/shameskweeb/keymaps/default/keymap.c
+++ keyboards/shameskweeb/keymaps/default/keymap.c
@@ -15,12 +15,7 @@ enum layers {
     SPACE_FN,
     CAPS_FN,
     FN,
-    AUSSIE,
-    TINY,
-    WIDE,
-    SCRIPT,
-    PAPER,
-    CLEAR,
+    CLEAR
 };
 
 bool isRecording = false;
@@ -37,8 +32,7 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 }
 
 #ifdef OLED_ENABLE
-bool oled_task_user(void) {
-    // Host Keyboard Layer Status
+void update_layer_status(void) {
     oled_write_P(PSTR("Layer: "), false);
 
     switch (get_highest_layer(layer_state)) {
@@ -48,26 +42,14 @@ bool oled_task_user(void) {
         case SPACE_FN:
             oled_write_P(PSTR("SPACE    "), false);
             break;
+        case SPACEFAT_FN:
+            oled_write_P(PSTR("SPACEFAT "), false);
+            break;
         case CAPS_FN:
             oled_write_P(PSTR("CAPS_FN  "), false);
             break;
         case FN:
             oled_write_P(PSTR("FN       "), false);
-            break;
-        case AUSSIE:
-            oled_write_P(PSTR("AUSSIE   "), false);
-            break;
-        case TINY:
-            oled_write_P(PSTR("TINY    "), false);
-            break;
-        case SCRIPT:
-            oled_write_P(PSTR("SCRIPT  "), false);
-            break;
-        case PAPER:
-            oled_write_P(PSTR("PAPEPT  "), false);
-            break;
-        case WIDE:
-            oled_write_P(PSTR("WIDE    "), false);
             break;
         default:
             oled_write_ln_P(PSTR("Undefined"), false);
@@ -155,60 +137,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______,               _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______, _______, _______, _______
         ),
 [FN]       = LAYOUT(
-        XXXXXXX,                        XXXXXXX, TO(AUSSIE), TO(TINY),TO(WIDE), TO(SCRIPT), TO(PAPER), XXXXXXX,                   UC_MOD, XXXXXXX, UC_M_MA, UC_M_EM,  XXXXXXX, XXXXXXX, KC_NLCK, KC_CAPS, KC_SLCK,
-        XXXXXXX, XXXXXXX,               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX,                        XXXXXXX, XXXXXXX,XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,                   UC_MOD, XXXXXXX, UC_M_MA, UC_M_EM,  XXXXXXX, XXXXXXX, KC_NLCK, KC_CAPS, KC_SLCK,
+        XXXXXXX, XXXXXXX,               DT_PRNT, DT_UP, DT_DOWN, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX,               XXXXXXX, KC_BTN1, KC_MS_U, KC_BTN2, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX,               XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_R, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT,
         XXXXXXX, XXXXXXX,               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX,  KC_WH_D,  KC_WH_U, XXXXXXX, KC_WH_U, XXXXXXX,
         XXXXXXX, XXXXXXX,               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                     XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX, KC_WH_L, KC_WH_D, KC_WH_R
-        ),
-[TINY]       = LAYOUT(
-        _______,                        TO(BASE), _______, _______,  _______,  _______, _______, _______,                   _______, _______, _______, _______,  _______, _______, _______, _______, _______,
-        _______,_______,                _______,  X(TINY_1),   X(TINY_2),    X(TINY_3),   X(TINY_4),   X(TINY_5),  X(TINY_6),  X(TINY_7),  X(TINY_8),   X(TINY_9),   X(TINY_0),   _______, _______,  _______, _______,
-        _______,_______,                _______,  X(TINY_Q),   X(TINY_W),    X(TINY_E),   X(TINY_R),   X(TINY_T),            X(TINY_Z),  X(TINY_U),   X(TINY_I),   X(TINY_O),   X(TINY_P),    _______, _______, _______,
-        _______,_______,          LT(2, KC_ESC), X(TINY_A),   X(TINY_S),    X(TINY_D),   X(TINY_F),   X(TINY_G),             X(TINY_H),  X(TINY_J),   X(TINY_K),   X(TINY_L),   _______, _______, _______, _______, _______,
-        _______,_______,               KC_LSFT, _______, X(TINY_Y),    X(TINY_X),   X(TINY_C),   X(TINY_V),  X(TINY_B),      X(TINY_N),  X(TINY_M),   _______,_______, _______, _______, _______, _______,
-        _______,_______,                _______, _______,_______, KC_SPC, MO(SPACE_FN),                                KC_SPC, KC_SPC, KC_RALT,MO(FN),  _______, _______, _______, _______
-        ),
-[WIDE]       = LAYOUT(
-        _______,                        TO(BASE), _______, _______,  _______,  _______, _______, _______,                   _______, _______, _______, _______,  _______, _______, _______, _______, _______,
-        _______,_______,                _______,  X(WIDE_1),   X(WIDE_2),    X(WIDE_3),   X(WIDE_4),   X(WIDE_5),  X(WIDE_6),  X(WIDE_7),  X(WIDE_8),   X(WIDE_9),   X(WIDE_0),   _______, _______,  _______, _______,
-        _______,_______,                _______,  XP(WIDE_S_Q, WIDE_L_Q),   XP(WIDE_S_W, WIDE_L_W),    XP(WIDE_S_E, WIDE_L_E),   XP(WIDE_S_R, WIDE_L_R),   XP(WIDE_S_T, WIDE_L_T),            XP(WIDE_S_Z, WIDE_L_Z),  XP(WIDE_S_U, WIDE_L_U),   XP(WIDE_S_I, WIDE_L_I),   XP(WIDE_S_O, WIDE_L_O),   XP(WIDE_S_P, WIDE_L_P),    _______, _______, _______,
-        _______,_______,          LT(2, KC_ESC), XP(WIDE_S_A, WIDE_L_A),   XP(WIDE_S_S, WIDE_L_S),    XP(WIDE_S_D, WIDE_L_D),   XP(WIDE_S_F, WIDE_L_F),   XP(WIDE_S_G, WIDE_L_G),             XP(WIDE_S_H, WIDE_L_H),  XP(WIDE_S_J, WIDE_L_J),   XP(WIDE_S_K, WIDE_L_K),   XP(WIDE_S_L, WIDE_L_L),   _______, _______, _______, _______, _______,
-        _______,_______,               KC_LSFT, _______, XP(WIDE_S_Y, WIDE_L_Y),    XP(WIDE_S_X, WIDE_L_X),   XP(WIDE_S_C, WIDE_L_C),   XP(WIDE_S_V, WIDE_L_V),  XP(WIDE_S_B, WIDE_L_B),      XP(WIDE_S_N, WIDE_L_N),  XP(WIDE_S_M, WIDE_L_M),   _______,_______, _______, _______, _______, _______,
-        _______,_______,                _______, _______,_______, KC_SPC, MO(SPACE_FN),                                KC_SPC, KC_SPC, KC_RALT,MO(FN),  _______, _______, _______, _______
-        ),
-[SCRIPT]       = LAYOUT(
-        _______,                        TO(BASE), _______, _______,  _______,  _______, _______, _______,                   _______, _______, _______, _______,  _______, _______, _______, _______, _______,
-        _______,_______,                _______,  _______,   _______,    _______,   _______,   _______,  _______,  _______,  _______,   _______,   _______,   _______, _______,  _______, _______,
-        _______,_______,                _______,  XP(SCRIPT_S_Q, SCRIPT_L_Q),   XP(SCRIPT_S_W, SCRIPT_L_W),    XP(SCRIPT_S_E, SCRIPT_L_E),   XP(SCRIPT_S_R, SCRIPT_L_R),   XP(SCRIPT_S_T, SCRIPT_L_T),            XP(SCRIPT_S_Z, SCRIPT_L_Z),  XP(SCRIPT_S_U, SCRIPT_L_U),   XP(SCRIPT_S_I, SCRIPT_L_I),   XP(SCRIPT_S_O, SCRIPT_L_O),   XP(SCRIPT_S_P, SCRIPT_L_P),    _______, _______, _______,
-        _______,_______,          LT(2, KC_ESC), XP(SCRIPT_S_A, SCRIPT_L_A),   XP(SCRIPT_S_S, SCRIPT_L_S),    XP(SCRIPT_S_D, SCRIPT_L_D),   XP(SCRIPT_S_F, SCRIPT_L_F),   XP(SCRIPT_S_G, SCRIPT_L_G),             XP(SCRIPT_S_H, SCRIPT_L_H),  XP(SCRIPT_S_J, SCRIPT_L_J),   XP(SCRIPT_S_K, SCRIPT_L_K),   XP(SCRIPT_S_L, SCRIPT_L_L),   _______, _______, _______, _______, _______,
-        _______,_______,               KC_LSFT, _______, XP(SCRIPT_S_Y, SCRIPT_L_Y),    XP(SCRIPT_S_X, SCRIPT_L_X),   XP(SCRIPT_S_C, SCRIPT_L_C),   XP(SCRIPT_S_V, SCRIPT_L_V),  XP(SCRIPT_S_B, SCRIPT_L_B),      XP(SCRIPT_S_N, SCRIPT_L_N),  XP(SCRIPT_S_M, SCRIPT_L_M),   _______,_______, _______, _______, _______, _______,
-        _______,_______,                _______, _______,_______, KC_SPC, MO(SPACE_FN),                                KC_SPC, KC_SPC, KC_RALT,MO(FN),  _______, _______, _______, _______
-        ),
-[AUSSIE]       = LAYOUT(
-        _______,                        TO(BASE), X(SNEK), _______,  _______,  _______, _______, _______,                   _______, _______, _______, _______,  _______, _______, _______, _______, _______,
-        _______,_______,                _______,  X(AUSSIE_1),   X(AUSSIE_2),    X(AUSSIE_3),   X(AUSSIE_4),   X(AUSSIE_5),  KC_9,  X(AUSSIE_7),  KC_8,   KC_6,   KC_0,   _______, _______,  _______, _______,
-        _______,_______,                _______,  KC_B,   X(AUSSIE_W),    X(AUSSIE_E),   X(AUSSIE_R),   X(AUSSIE_T),            KC_Y,  KC_N,   X(AUSSIE_I),   KC_O,   KC_D,    _______, _______, _______,
-        _______,_______,          LT(2, KC_ESC), X(AUSSIE_A),   KC_S,    KC_P,   X(AUSSIE_F),   X(AUSSIE_G),             X(AUSSIE_H),  X(AUSSIE_J),   X(AUSSIE_K),   KC_L,   _______, _______, _______, _______, _______,
-        _______,_______,               KC_LSFT, _______, X(AUSSIE_Y),    X(AUSSIE_X),   X(AUSSIE_C),   X(AUSSIE_V),  KC_Q,      KC_U,  X(AUSSIE_M),   _______,_______, _______, _______, _______, _______,
-        _______,_______,                _______, _______,_______, KC_SPC, MO(SPACE_FN),                                KC_SPC, KC_SPC, KC_RALT,MO(FN),  _______, _______, _______, _______
-        ),
-
-[PAPER]       = LAYOUT(
-        _______,                        TO(BASE), _______, _______,  _______,  _______, _______, _______,                   _______, _______, _______, _______,  _______, _______, _______, _______, _______,
-        _______,_______,                _______,  _______,   _______,    _______,   _______,   _______,  _______,  _______,  _______,   _______,   _______,   _______, _______,  _______, _______,
-        _______,_______,                _______,  X(PAPER_Q),   X(PAPER_W),    X(PAPER_E),   X(PAPER_R),   X(PAPER_T),            X(PAPER_Z),  X(PAPER_U),   X(PAPER_I),   X(PAPER_O),   X(PAPER_P),    _______, _______, _______,
-        _______,_______,          LT(2, KC_ESC), X(PAPER_A),   X(PAPER_S),    X(PAPER_D),   X(PAPER_F),   X(PAPER_G),             X(PAPER_H),  X(PAPER_J),   X(PAPER_K),   X(PAPER_L),   _______, _______, _______, _______, _______,
-        _______,_______,               KC_LSFT, _______, X(PAPER_Y),    X(PAPER_X),   X(PAPER_C),   X(PAPER_V),  X(PAPER_B),      X(PAPER_N),  X(PAPER_M),   _______,_______, _______, _______, _______, _______,
-        _______,_______,                _______, _______,_______, KC_SPC, MO(SPACE_FN),                                KC_SPC, KC_SPC, KC_RALT,MO(FN),  _______, _______, _______, _______
-        ),
+        )
 // [CLEAR]       = LAYOUT(
-//         _______,                        _______, _______, _______,  _______,  _______, _______, _______,                   _______, _______, _______, _______,  _______, _______, _______, _______, KC_SLCK,
-//         _______, _______,               _______, _______, _______, _______, _______, _______, _______,                   _______, _______, _______,  _______, _______, _______, _______, _______,
+//         _______,                        _______, _______, _______,  _______,  _______, _______, _______,                _______, _______, _______,  _______,  _______, _______, _______, _______, KC_SLCK,
+//         _______, _______,               _______, _______, _______, _______, _______, _______, _______,                  _______, _______, _______,  _______,  _______, _______, _______, _______,
 //         _______, _______,               _______, _______,_______, _______, _______, _______,                            _______, _______, _______,  _______,  _______, _______, _______, _______,
-//         _______, _______,               _______, _______, _______, _______, _______, _______,                            _______, _______, _______,  _______,  _______, _______, _______, _______, QK_BOOT,
-//         _______, _______,               _______, _______, _______, _______, _______, _______, _______,                   _______, _______, _______,  _______,  _______, _______, _______, _______,
-//         _______, _______,               _______, _______, _______, _______, _______,                                     _______, _______, _______,  _______,  _______, _______, _______, _______
+//         _______, _______,               _______, _______, _______, _______, _______, _______,                           _______, _______, _______,  _______,  _______, _______, _______, _______, QK_BOOT,
+//         _______, _______,               _______, _______, _______, _______, _______, _______, _______,                  _______, _______, _______,  _______,  _______, _______, _______, _______,
+//         _______, _______,               _______, _______, _______, _______, _______,                                    _______, _______, _______,  _______,  _______, _______, _______, _______
 //         )
 };
